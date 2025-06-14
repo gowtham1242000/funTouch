d5f2d656af0cc4e1feaa1feb0991e664f789482a
Gowtham <158307045+gowtham1242000@users.noreply.github.com>
2025-06-14 14:30:58 +0530

    Delete services/controllers directory

--- services/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-const User = require("../models/User");
-
-const registerUser = async (req, res) => {
-  const { name, email, phone, preferences } = req.body;
-  const user = await User.create({ name, preferences, email, phone });
-  res.status(201).json(user);
-};
-module.exports = { registerUser };
